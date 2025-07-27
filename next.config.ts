9c417a347839d22392d46ebcd9d510836477d619
RainPPR <41764766+RainPPR@users.noreply.github.com>
2025-07-27 17:56:39 +0800

    Update

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  cleanDistDir: true,
-  basePath: process.env.PAGES_BASE_PATH || '',
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
+  reactStrictMode: true
 };
 
 export default nextConfig;
